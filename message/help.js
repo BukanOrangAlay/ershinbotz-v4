eee8de9a59daa9b0a390d05b2a919b2c23093ee6
suka-suka <50833454+p3mu14-ui@users.noreply.github.com>
2022-02-04 23:38:08 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,879 @@
+exports.gameMenu = (prefix) => {
+    return `
+╭─⬣「 *GAME MENU* 」⬣ 
+│❏ヅ ${prefix}limitgame
+│❏ヅ ${prefix}slot
+│❏ヅ ${prefix}gelud *@tag*
+│❏ヅ ${prefix}tictactoe *@tag*
+│❏ヅ ${prefix}siapaaku
+│❏ヅ ${prefix}family100
+│❏ヅ ${prefix}kuismath
+│❏ヅ ${prefix}asahotak
+│❏ヅ ${prefix}tebaklirik
+│❏ヅ ${prefix}tebaklagu
+│❏ヅ ${prefix}tebakkata
+│❏ヅ ${prefix}susunkata
+│❏ヅ ${prefix}kimiakuis
+│❏ヅ ${prefix}caklontong
+│❏ヅ ${prefix}tebakjenaka
+│❏ヅ ${prefix}tebakanime
+│❏ヅ ${prefix}tebaktebakan
+│❏ヅ ${prefix}tebakgambar
+│❏ヅ ${prefix}tebakumur
+│❏ヅ ${prefix}tebakbendera
+│❏ヅ ${prefix}suit *batu/kertas/gunting*
+│❏ヅ ${prefix}math
+└⬣
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+╭─⬣「 *FUN MENU* 」⬣
+│❏ヅ ${prefix}mining
+│❏ヅ ${prefix}cekwatak
+│❏ヅ ${prefix}cekmati [nama]
+│❏ヅ ${prefix}wangy [nama]
+│❏ヅ ${prefix}citacita
+│❏ヅ ${prefix}toxic
+│❏ヅ ${prefix}truth
+│❏ヅ ${prefix}dare
+│❏ヅ ${prefix}apakah
+│❏ヅ ${prefix}bisakah
+│❏ヅ ${prefix}kapankah
+│❏ヅ ${prefix}rate
+│❏ヅ ${prefix}jadian
+│❏ヅ ${prefix}cantik
+│❏ヅ ${prefix}ganteng
+│❏ヅ ${prefix}beban
+│❏ヅ ${prefix}babi
+│❏ヅ ${prefix}cekganteng
+│❏ヅ ${prefix}cekcantik
+└⬣
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+╭─⬣「 *OTHER MENU* 」⬣
+│❏ヅ ${prefix}brainly *query*
+│❏ヅ ${prefix}cerpen
+│❏ヅ ${prefix}shopee *product*
+│❏ヅ ${prefix}playstore *query*
+│❏ヅ ${prefix}ssweb *query*
+│❏ヅ ${prefix}google *query*
+│❏ヅ ${prefix}image *query*
+│❏ヅ ${prefix}pinterest *query*
+│❏ヅ ${prefix}nulis *teks*
+│❏ヅ ${prefix}iguser *ussername*
+│❏ヅ ${prefix}igstalk *username*
+│❏ヅ ${prefix}githubstalk *username*
+│❏ヅ ${prefix}tiktokstalk *ussername*
+│❏ヅ ${prefix}img2url *reply foto*
+│❏ヅ ${prefix}ytsearch *query*
+└⬣
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+╭─⬣「 *STIKER MENU* 」⬣
+│❏ヅ ${prefix}attp *teks*
+│❏ヅ ${prefix}ttp2 *teks*
+│❏ヅ ${prefix}ttp3 *teks*
+│❏ヅ ${prefix}ttp4 *teks*
+│❏ヅ ${prefix}amongus *teks*
+│❏ヅ ${prefix}dadu
+│❏ヅ ${prefix}doge
+│❏ヅ ${prefix}patrick
+│❏ヅ ${prefix}gura
+│❏ヅ ${prefix}stickeranime
+│❏ヅ ${prefix}semoji *emoji*
+│❏ヅ ${prefix}sticker *reply foto/video*
+│❏ヅ ${prefix}smeme *teks|teks*
+│❏ヅ ${prefix}swm *pack|author*
+│❏ヅ ${prefix}take *pack|author* 
+│❏ヅ ${prefix}tovideo *reply sgif*
+│❏ヅ ${prefix}toimg
+└⬣
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+╭─⬣「 *WIBU MENU* 」⬣
+│❏ヅ ${prefix}loli
+│❏ヅ ${prefix}manga
+│❏ヅ ${prefix}anime 
+│❏ヅ ${prefix}lolivideo
+│❏ヅ ${prefix}husbu
+│❏ヅ ${prefix}waifu
+│❏ヅ ${prefix}milf
+│❏ヅ ${prefix}neko
+│❏ヅ ${prefix}kanna
+│❏ヅ ${prefix}sagiri
+│❏ヅ ${prefix}hentai
+│❏ヅ ${prefix}wallnime
+│❏ヅ ${prefix}kusonime
+│❏ヅ ${prefix}megumin
+│❏ヅ ${prefix}otakudesu
+│❏ヅ ${prefix}doujindesu
+│❏ヅ ${prefix}storyanime
+│❏ヅ ${prefix}otakuongoing
+│❏ヅ ${prefix}nhentai *code*
+│❏ヅ ${prefix}nekopoi *link*
+└⬣
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+╭─⬣「 *OWNER MENU* 」⬣
+│❏ヅ ${prefix}bc *teks*
+│❏ヅ ${prefix}term
+│❏ヅ ${prefix}eval
+│❏ヅ ${prefix}reset
+│❏ヅ ${prefix}clearall
+│❏ヅ ${prefix}leaveall
+│❏ヅ ${prefix}join *link gc*
+│❏ヅ ${prefix}shutdown
+│❏ヅ ${prefix}getquoted
+│❏ヅ ${prefix}addupdate *fiturnya*
+│❏ヅ ${prefix}exif *nama|author*
+│❏ヅ ${prefix}sewa add/del *waktunya*
+│❏ヅ ${prefix}premium add @tag|nomor
+│❏ヅ ${prefix}premium del @tag|nomor
+└⬣
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+╭─⬣「 *GRUP MENU* 」⬣    
+│❏ヅ ${prefix}groupsetting
+│❏ヅ ${prefix}afk *alasan*
+│❏ヅ ${prefix}ceksewa
+│❏ヅ ${prefix}kickall
+│❏ヅ ${prefix}infogrup
+│❏ヅ ${prefix}promote
+│❏ヅ ${prefix}demote
+│❏ヅ ${prefix}listonline
+│❏ヅ ${prefix}tagall *teks*
+│❏ヅ ${prefix}leave
+│❏ヅ ${prefix}kick *reply*
+│❏ヅ ${prefix}add *+62xxxxxx*
+│❏ヅ ${prefix}setnamegc
+│❏ヅ ${prefix}setppgc
+│❏ヅ ${prefix}setdeskgc
+│❏ヅ ${prefix}sider *reply chat bot*
+│❏ヅ ${prefix}hidetag *teks/reply teks*
+│❏ヅ ${prefix}linkgc
+└⬣
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+╭─⬣「 *DOWNLOADER* 」⬣
+│❏ヅ ${prefix}fbdl 
+│❏ヅ ${prefix}igdl 
+│❏ヅ ${prefix}igdl2 
+│❏ヅ ${prefix}twitter 
+│❏ヅ ${prefix}tiktok 
+│❏ヅ ${prefix}play 
+│❏ヅ ${prefix}ythd 
+│❏ヅ ${prefix}ytmp3 
+│❏ヅ ${prefix}ytmp4 
+│❏ヅ ${prefix }soundcloud 
+│❏ヅ ${prefix}tiktoknowm 
+│❏ヅ ${prefix}tiktokaudio
+│❏ヅ ${prefix}mediafire 
+│❏ヅ ${prefix}nhentaipdf *code*
+└⬣
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+╭─⬣「 *RULES BOT* 」⬣
+1. Jangan Toxic Ke Bot. 
+Sanksi: *PERMANENT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan Spam Fitur Bot.
+Sanksi: *PERMANENT BLOCK*
+
+4. Jangan Culik Bot.
+Sanksi: *GRUP DI SERANG PAKAI BUG/VIRUS*
+
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai!
+└⬣
+`
+}
+exports.iklanBot = (prefix) => {
+return`         𝗜𝗞𝗟𝗔𝗡 𝗕𝗢𝗧𝗭
+      
+KEUNTUNGAN BOTZ
+▢ BISA JAGA GRUP DARI LINK
+▢ BISA BIKIN STIKER TEKS,VIDIO DAN GAMBAR
+▢ BISA NYAPA TAMU YG MASUK
+▢ BISA PLAY AUDIO DAN VIDIO
+▢ BISA JADI ADMIN BERBER
+
+KELEBIHAN BOTZ
+▢ ON 24JAM KALAU GK DELAY
+▢ SELALU UPDATE FITUR (INSYAALLAH)
+▢ BOT NO PASARAN
+
+LIST HARGA SEWA BOTZ
+▢ PERHARI 2K
+▢ PERMINGGU 5K
+▢ PERBULAN 15K
+▢ PERMANEN 40K
+MINAT HIBUNGI OWNER
+wa.me/6289627655877
+
+KUMPUL DISNI BUAT PEMULA BOT
+https://chat.whatsapp.com/Loc6xVYLGnwHm9vuz6hZe7`
+}
+exports.infoMenu = (prefix) => {
+return `
+╭─⬣「 *INFO MENU* 」⬣
+│❏ヅ ${prefix}update
+│❏ヅ ${prefix}level
+│❏ヅ ${prefix}rules
+│❏ヅ ${prefix}profile
+│❏ヅ ${prefix}waktu
+│❏ヅ ${prefix}botstat
+│❏ヅ ${prefix}sewabot
+│❏ヅ ${prefix}listsewa
+│❏ヅ ${prefix}owner
+│❏ヅ ${prefix}ping
+│❏ヅ ${prefix}runtime
+│❏ヅ ${prefix}donasi
+│❏ヅ ${prefix}leaderboard
+│❏ヅ ${prefix}cekpremium
+│❏ヅ ${prefix}listpremium
+│❏ヅ ${prefix}getpp
+│❏ヅ ${prefix}setpp
+│❏ヅ ${prefix}bugreport *keluhan*
+└⬣
+`
+} 
+exports.islamMenu = (prefix) => {
+return `
+╭─⬣「 *ISLAM MENU* 」⬣  
+│❏ヅ ${prefix}kisahnabi
+│❏ヅ ${prefix}jadwalsholat
+│❏ヅ ${prefix}alquran
+│❏ヅ ${prefix}asmaulhusna
+│❏ヅ ${prefix}alquranaudio
+│❏ヅ ${prefix}listsurah
+└⬣
+`
+}
+exports.sertiMenu = (prefix) => {
+return `
+╭─⬣「 *SERTI MENU* 」⬣  
+│❏ヅ ${prefix}sertitolol 
+│❏ヅ ${prefix}bucinserti
+│❏ヅ ${prefix}fuckboy
+│❏ヅ ${prefix}fuckgirl
+│❏ヅ ${prefix}badboy
+│❏ヅ ${prefix}badgirl
+│❏ヅ ${prefix}goodboy
+│❏ヅ ${prefix}goodgirl
+└⬣
+*MASIH TAHAP PENGEMBANGAN*
+`
+}
+exports.ceritaMenu = (prefix) => {
+return`
+╭─⬣「 *CERITA MENU* 」⬣   
+│❏ヅ ${prefix}cerpen
+│❏ヅ ${prefix}horor
+│❏ヅ ${prefix}quotesdilan
+│❏ヅ ${prefix}pantun
+│❏ヅ ${prefix}faktaunik
+│❏ヅ ${prefix}bucin
+│❏ヅ ${prefix}quotes
+│❏ヅ ${prefix}quotesanime
+└⬣
+`
+}
+exports.makerMenu = (prefix) => {
+return`
+╭─⬣「 *MAKER MENU* 」⬣
+│❏ヅ ${prefix}tahta
+│❏ヅ ${prefix}
+│❏ヅ ${prefix}cup
+│❏ヅ ${prefix}cup1
+│❏ヅ ${prefix}coffe
+│❏ヅ ${prefix}birthdaycake
+│❏ヅ ${prefix}metallogo
+│❏ヅ ${prefix}lighttext
+│❏ヅ ${prefix}halloween
+│❏ヅ ${prefix}vampire
+│❏ヅ ${prefix}matrix
+│❏ヅ ${prefix}googletxt
+│❏ヅ ${prefix}spiderman
+│❏ヅ ${prefix}express
+└⬣
+*SISANYA NYUSUL MASIH TAHAP PENGEMBANGAM FITUR*
+`
+}
+exports.warMenu = (prefix) => {
+return`
+╭─⬣「 *WAR MENU* 」⬣
+│❏ヅ ${prefix}pvp
+└⬣
+`
+}
+exports.hewanMenu = (prefix) => {
+return`
+*SEDANGMAINTENANCE / PROSES PEMBUATAN*
+`
+}
+exports.dewasaMenu = (prefix) => {
+return`
+╭─⬣「 *DEWASA MENU* 」⬣
+│❏ヅ ${prefix}xnxx *link*
+│❏ヅ ${prefix}xnxxsearch
+│❏ヅ ${prefix}asupan
+│❏ヅ ${prefix}randombokep
+│❏ヅ ${prefix}desah
+│❏ヅ ${prefix}desah1
+│❏ヅ ${prefix}desah2
+│❏ヅ ${prefix}desah3
+│❏ヅ ${prefix}desah4
+│❏ヅ ${prefix}desah5
+│❏ヅ ${prefix}desah6
+└⬣
+
+NOTE:
+_ANAK DI BAWAH UMUR 18 TAHUN JANGAN GUNAKAN FITUR INI NANTI LU SANGE🗿_
+`}
+exports.soundMenu = (prefix) => {
+return`
+╭─⬣「 *SOUND MENU* 」⬣
+│❏ヅ ${prefix}sound1
+│❏ヅ ${prefix}sound2
+│❏ヅ ${prefix}sound3
+│❏ヅ ${prefix}sound4
+│❏ヅ ${prefix}sound5
+│❏ヅ ${prefix}sound6
+│❏ヅ ${prefix}sound7
+│❏ヅ ${prefix}sound8
+│❏ヅ ${prefix}sound9
+│❏ヅ ${prefix}sound10
+│❏ヅ ${prefix}sound11
+│❏ヅ ${prefix}sound12
+│❏ヅ ${prefix}sound13
+│❏ヅ ${prefix}sound14
+│❏ヅ ${prefix}sound15
+│❏ヅ ${prefix}sound16
+│❏ヅ ${prefix}sound17
+│❏ヅ ${prefix}sound18
+│❏ヅ ${prefix}sound19
+│❏ヅ ${prefix}sound20
+│❏ヅ ${prefix}sound21
+│❏ヅ ${prefix}sound22
+│❏ヅ ${prefix}sound23
+│❏ヅ ${prefix}sound24
+│❏ヅ ${prefix}sound25
+│❏ヅ ${prefix}sound26
+│❏ヅ ${prefix}sound27
+│❏ヅ ${prefix}sound28
+│❏ヅ ${prefix}sound29
+│❏ヅ ${prefix}sound30
+│❏ヅ ${prefix}sound31
+│❏ヅ ${prefix}sound32
+│❏ヅ ${prefix}sound33
+│❏ヅ ${prefix}sound34
+│❏ヅ ${prefix}sound35
+│❏ヅ ${prefix}sound36
+│❏ヅ ${prefix}sound37
+│❏ヅ ${prefix}sound38
+│❏ヅ ${prefix}sound39
+│❏ヅ ${prefix}sound40
+│❏ヅ ${prefix}sound41
+│❏ヅ ${prefix}sound42
+│❏ヅ ${prefix}sound43
+│❏ヅ ${prefix}sound44
+│❏ヅ ${prefix}sound45
+│❏ヅ ${prefix}sound46
+│❏ヅ ${prefix}sound47
+│❏ヅ ${prefix}sound48
+│❏ヅ ${prefix}sound49
+│❏ヅ ${prefix}sound50
+└⬣
+`}
+exports.toolsMenu = (prefix) => {
+return`
+╭─⬣「 *TOOLS MENU* 」⬣   
+│❏ヅ ${prefix}addvn
+│❏ヅ ${prefix}listvn
+│❏ヅ ${prefix}getvn
+│❏ヅ ${prefix}addimg
+│❏ヅ ${prefix}listimg
+│❏ヅ ${prefix}getimg
+│❏ヅ ${prefix}addvid
+│❏ヅ ${prefix}listvid
+│❏ヅ ${prefix}getvid
+│❏ヅ ${prefix}addstik
+│❏ヅ ${prefix}liststik
+│❏ヅ ${prefix}getstik
+│❏ヅ ${prefix}tupai
+└⬣
+`}
+exports.allMenu = (prefix) => {
+    return`━━━━━『ErshinBotz V2』━━━━━
+
+━━━━━ *INFO USER* ━━━━━
+❏ NAMA : *${pushname}*
+❏ API : *@${sender.split('@')[0]}*
+❏ STATUS : *${isOwner ? 'OWNER' : isPremium ? 'Premium' : 'Gratisan'}*
+❏ LIMIT : *${isPremium ? 'Unlimited' : `${gcount}`}*
+
+━━━━━ *INFO BOTZ* ━━━━━
+❏ NAMA : *ERSHIN BOTZ*
+❏ API : *@6285163677123*
+❏ OWNER : *P3mu14-ui*
+❏ API : *@6289627655877*
+❏ AKTIF : *${runtime(process.uptime())}*
+❏ BATERAI : *${baterai}%*
+❏ PREIFIX : *『multi prefix』*
+❏ TOTAL FITUR : *300+*
+
+   ━━━━━ *ALLMENU* ━━━━━
+
+╭─⬣「 *INFO MENU* 」⬣
+│❏ヅ ${prefix}update
+│❏ヅ ${prefix}level
+│❏ヅ ${prefix}rules
+│❏ヅ ${prefix}profile
+│❏ヅ ${prefix}waktu
+│❏ヅ ${prefix}botstat
+│❏ヅ ${prefix}sewabot
+│❏ヅ ${prefix}listsewa
+│❏ヅ ${prefix}owner
+│❏ヅ ${prefix}ping
+│❏ヅ ${prefix}runtime
+│❏ヅ ${prefix}donasi
+│❏ヅ ${prefix}leaderboard
+│❏ヅ ${prefix}cekpremium
+│❏ヅ ${prefix}listpremium
+│❏ヅ ${prefix}getpp
+│❏ヅ ${prefix}setpp
+│❏ヅ ${prefix}bugreport *keluhan*
+└⬣
+
+╭─⬣「 *GRUP MENU* 」⬣    
+│❏ヅ ${prefix}groupsetting
+│❏ヅ ${prefix}afk *alasan*
+│❏ヅ ${prefix}ceksewa
+│❏ヅ ${prefix}kickall
+│❏ヅ ${prefix}infogrup
+│❏ヅ ${prefix}promote
+│❏ヅ ${prefix}demote
+│❏ヅ ${prefix}listonline
+│❏ヅ ${prefix}tagall *teks*
+│❏ヅ ${prefix}leave
+│❏ヅ ${prefix}kick *reply*
+│❏ヅ ${prefix}add *+62xxxxxx*
+│❏ヅ ${prefix}setnamegc
+│❏ヅ ${prefix}setppgc
+│❏ヅ ${prefix}setdeskgc
+│❏ヅ ${prefix}sider *reply chat bot*
+│❏ヅ ${prefix}hidetag *teks/reply teks*
+│❏ヅ ${prefix}linkgc
+└⬣
+
+╭─⬣「 *GAME MENU* 」⬣ 
+│❏ヅ ${prefix}limitgame
+│❏ヅ ${prefix}slot
+│❏ヅ ${prefix}gelud *@tag*
+│❏ヅ ${prefix}tictactoe *@tag*
+│❏ヅ ${prefix}siapaaku
+│❏ヅ ${prefix}family100
+│❏ヅ ${prefix}kuismath
+│❏ヅ ${prefix}asahotak
+│❏ヅ ${prefix}tebaklirik
+│❏ヅ ${prefix}tebaklagu
+│❏ヅ ${prefix}tebakkata
+│❏ヅ ${prefix}susunkata
+│❏ヅ ${prefix}kimiakuis
+│❏ヅ ${prefix}caklontong
+│❏ヅ ${prefix}tebakjenaka
+│❏ヅ ${prefix}tebakanime
+│❏ヅ ${prefix}tebaktebakan
+│❏ヅ ${prefix}tebakgambar
+│❏ヅ ${prefix}tebakumur
+│❏ヅ ${prefix}tebakbendera
+│❏ヅ ${prefix}suit *batu/kertas/gunting*
+│❏ヅ ${prefix}math
+└⬣
+
+╭─⬣「 *ISLAM MENU* 」⬣  
+│❏ヅ ${prefix}kisahnabi
+│❏ヅ ${prefix}jadwalsholat
+│❏ヅ ${prefix}alquran
+│❏ヅ ${prefix}asmaulhusna
+│❏ヅ ${prefix}alquranaudio
+│❏ヅ ${prefix}listsurah
+└⬣
+
+╭─⬣「 *DOWNLOADER* 」⬣
+│❏ヅ ${prefix}fbdl 
+│❏ヅ ${prefix}igdl 
+│❏ヅ ${prefix}igdl2 
+│❏ヅ ${prefix}twitter 
+│❏ヅ ${prefix}tiktok 
+│❏ヅ ${prefix}play 
+│❏ヅ ${prefix}ythd 
+│❏ヅ ${prefix}ytmp3 
+│❏ヅ ${prefix}ytmp4 
+│❏ヅ ${prefix }soundcloud 
+│❏ヅ ${prefix}tiktoknowm 
+│❏ヅ ${prefix}tiktokaudio
+│❏ヅ ${prefix}mediafire 
+│❏ヅ ${prefix}nhentaipdf *code*
+└⬣
+
+╭─⬣「 *MAKER MENU* 」⬣
+│❏ヅ ${prefix}tahta
+│❏ヅ ${prefix}
+│❏ヅ ${prefix}cup
+│❏ヅ ${prefix}cup1
+│❏ヅ ${prefix}coffe
+│❏ヅ ${prefix}birthdaycake
+│❏ヅ ${prefix}metallogo
+│❏ヅ ${prefix}lighttext
+│❏ヅ ${prefix}halloween
+│❏ヅ ${prefix}vampire
+│❏ヅ ${prefix}matrix
+│❏ヅ ${prefix}googletxt
+│❏ヅ ${prefix}spiderman
+│❏ヅ ${prefix}express
+└⬣
+
+╭─⬣「 *SERTI MENU* 」⬣  
+│❏ヅ ${prefix}sertitolol 
+│❏ヅ ${prefix}bucinserti
+│❏ヅ ${prefix}fuckboy
+│❏ヅ ${prefix}fuckgirl
+│❏ヅ ${prefix}badboy
+│❏ヅ ${prefix}badgirl
+│❏ヅ ${prefix}goodboy
+│❏ヅ ${prefix}goodgirl
+└⬣
+
+╭─⬣「 *STIKER MENU* 」⬣
+│❏ヅ ${prefix}attp *teks*
+│❏ヅ ${prefix}ttp2 *teks*
+│❏ヅ ${prefix}ttp3 *teks*
+│❏ヅ ${prefix}ttp4 *teks*
+│❏ヅ ${prefix}amongus *teks*
+│❏ヅ ${prefix}dadu
+│❏ヅ ${prefix}doge
+│❏ヅ ${prefix}patrick
+│❏ヅ ${prefix}gura
+│❏ヅ ${prefix}stickeranime
+│❏ヅ ${prefix}semoji *emoji*
+│❏ヅ ${prefix}sticker *reply foto/video*
+│❏ヅ ${prefix}smeme *teks|teks*
+│❏ヅ ${prefix}swm *pack|author*
+│❏ヅ ${prefix}take *pack|author* 
+│❏ヅ ${prefix}tovideo *reply sgif*
+│❏ヅ ${prefix}toimg
+└⬣
+
+╭─⬣「 *SOUND MENU* 」⬣
+│❏ヅ ${prefix}sound1
+│❏ヅ ${prefix}sound2
+│❏ヅ ${prefix}sound3
+│❏ヅ ${prefix}sound4
+│❏ヅ ${prefix}sound5
+│❏ヅ ${prefix}sound6
+│❏ヅ ${prefix}sound7
+│❏ヅ ${prefix}sound8
+│❏ヅ ${prefix}sound9
+│❏ヅ ${prefix}sound10
+│❏ヅ ${prefix}sound11
+│❏ヅ ${prefix}sound12
+│❏ヅ ${prefix}sound13
+│❏ヅ ${prefix}sound14
+│❏ヅ ${prefix}sound15
+│❏ヅ ${prefix}sound16
+│❏ヅ ${prefix}sound17
+│❏ヅ ${prefix}sound18
+│❏ヅ ${prefix}sound19
+│❏ヅ ${prefix}sound20
+│❏ヅ ${prefix}sound21
+│❏ヅ ${prefix}sound22
+│❏ヅ ${prefix}sound23
+│❏ヅ ${prefix}sound24
+│❏ヅ ${prefix}sound25
+│❏ヅ ${prefix}sound26
+│❏ヅ ${prefix}sound27
+│❏ヅ ${prefix}sound28
+│❏ヅ ${prefix}sound29
+│❏ヅ ${prefix}sound30
+│❏ヅ ${prefix}sound31
+│❏ヅ ${prefix}sound32
+│❏ヅ ${prefix}sound33
+│❏ヅ ${prefix}sound34
+│❏ヅ ${prefix}sound35
+│❏ヅ ${prefix}sound36
+│❏ヅ ${prefix}sound37
+│❏ヅ ${prefix}sound38
+│❏ヅ ${prefix}sound39
+│❏ヅ ${prefix}sound40
+│❏ヅ ${prefix}sound41
+│❏ヅ ${prefix}sound42
+│❏ヅ ${prefix}sound43
+│❏ヅ ${prefix}sound44
+│❏ヅ ${prefix}sound45
+│❏ヅ ${prefix}sound46
+│❏ヅ ${prefix}sound47
+│❏ヅ ${prefix}sound48
+│❏ヅ ${prefix}sound49
+│❏ヅ ${prefix}sound50
+└⬣
+
+╭─⬣「 *SOUND2 MENU* 」⬣
+│❏ヅ ${prefix}tupai
+│❏ヅ ${prefix}robot
+│❏ヅ ${prefix}gemuk
+│❏ヅ ${prefix}bass
+│❏ヅ ${prefix}terbalik
+│❏ヅ ${prefix}tts
+│❏ヅ ${prefix}detikvn
+│❏ヅ ${prefix}detikvideo
+│❏ヅ ${prefix}toimg
+│❏ヅ ${prefix}tovideo
+│❏ヅ ${prefix}tomp3
+└⬣
+
+╭─⬣「 *STATUS MENU* 」⬣
+│❏ヅ ${prefix}upswteks *teks*
+│❏ヅ ${prefix}upswlokasi *teks*
+│❏ヅ ${prefix}upswvideo *video*
+│❏ヅ ${prefix}upswimage *image*
+│❏ヅ ${prefix}upswstiker *stiker*
+│❏ヅ ${prefix}upswvoice *voice*
+│❏ヅ ${prefix}upswaudio *audio*
+│❏ヅ ${prefix}upswgif *gif*
+└⬣
+
+╭─⬣「 *DEWASA MENU* 」⬣
+│❏ヅ ${prefix}xnxx *link*
+│❏ヅ ${prefix}xnxxsearch
+│❏ヅ ${prefix}asupan
+│❏ヅ ${prefix}randombokep
+│❏ヅ ${prefix}desah
+│❏ヅ ${prefix}desah1
+│❏ヅ ${prefix}desah2
+│❏ヅ ${prefix}desah3
+│❏ヅ ${prefix}desah4
+│❏ヅ ${prefix}desah5
+│❏ヅ ${prefix}desah6
+└⬣
+
+╭─⬣「 *OTHER MENU* 」⬣
+│❏ヅ ${prefix}brainly *query*
+│❏ヅ ${prefix}cerpen
+│❏ヅ ${prefix}shopee *product*
+│❏ヅ ${prefix}playstore *query*
+│❏ヅ ${prefix}ssweb *query*
+│❏ヅ ${prefix}google *query*
+│❏ヅ ${prefix}image *query*
+│❏ヅ ${prefix}pinterest *query*
+│❏ヅ ${prefix}nulis *teks*
+│❏ヅ ${prefix}iguser *ussername*
+│❏ヅ ${prefix}igstalk *username*
+│❏ヅ ${prefix}githubstalk *username*
+│❏ヅ ${prefix}tiktokstalk *ussername*
+│❏ヅ ${prefix}img2url *reply foto*
+│❏ヅ ${prefix}ytsearch *query*
+└⬣
+
+
+╭─⬣「 *WAR MENU* 」⬣
+│❏ヅ ${prefix}pvp
+└⬣
+
+╭─⬣「 *HIBURAN MENU* 」⬣
+│❏ヅ ${prefix}katailham
+│❏ヅ ${prefix}quotes
+│❏ヅ ${prefix}quotesanime
+│❏ヅ ${prefix}quotesislami
+│❏ヅ ${prefix}quotesdilan
+│❏ヅ ${prefix}pantun
+│❏ヅ ${prefix}bucin
+│❏ヅ ${prefix}faktaunik
+└⬣
+
+╭─⬣「 *HEWAN MENU* 」⬣
+│❏ヅ ${prefix}buaya
+│❏ヅ ${prefix}elang
+│❏ヅ ${prefix}ayam
+│❏ヅ ${prefix}burung
+│❏ヅ ${prefix}burung hantu
+│❏ヅ ${prefix}burung gagak
+│❏ヅ ${prefix}burung puyuh
+│❏ヅ ${prefix}bebek
+│❏ヅ ${prefix}angsa
+│❏ヅ ${prefix}sapi
+│❏ヅ ${prefix}kuda
+│❏ヅ ${prefix}badak
+│❏ヅ ${prefix}kambing
+│❏ヅ ${prefix}unta
+│❏ヅ ${prefix}kucing
+│❏ヅ ${prefix}anjing
+│❏ヅ ${prefix}harimau
+│❏ヅ ${prefix}singa
+│❏ヅ ${prefix}monyet
+│❏ヅ ${prefix}babi
+│❏ヅ ${prefix}ikan
+│❏ヅ ${prefix}ikan hiu
+│❏ヅ ${prefix}ikan kembung
+│❏ヅ ${prefix}ikan laga
+│❏ヅ ${prefix}tikus
+│❏ヅ ${prefix}kelinci
+│❏ヅ ${prefix}ular
+│❏ヅ ${prefix}siput
+│❏ヅ ${prefix}domba
+│❏ヅ ${prefix}katak
+│❏ヅ ${prefix}naga
+│❏ヅ ${prefix}laba-laba
+│❏ヅ ${prefix}kupu-kupu
+│❏ヅ ${prefix}kalajengking
+│❏ヅ ${prefix}kingkong
+│❏ヅ ${prefix}jerapa
+│❏ヅ ${prefix}gajah
+│❏ヅ ${prefix}serigala
+│❏ヅ ${prefix}cikcak
+│❏ヅ ${prefix}pinguin
+│❏ヅ ${prefix}kepiting
+│❏ヅ ${prefix}ubur-ubur
+│❏ヅ ${prefix}lebah
+│❏ヅ ${prefix}kura-kura
+│❏ヅ ${prefix}banteng
+│❏ヅ ${prefix}kelalawar
+└⬣
+
+╭─⬣「 *TOOLS MENU* 」⬣   
+│❏ヅ ${prefix}addvn
+│❏ヅ ${prefix}listvn
+│❏ヅ ${prefix}getvn
+│❏ヅ ${prefix}addimg
+│❏ヅ ${prefix}listimg
+│❏ヅ ${prefix}getimg
+│❏ヅ ${prefix}addvid
+│❏ヅ ${prefix}listvid
+│❏ヅ ${prefix}getvid
+│❏ヅ ${prefix}addstik
+│❏ヅ ${prefix}liststik
+│❏ヅ ${prefix}getstik
+│❏ヅ ${prefix}tupai
+└⬣
+
+╭─⬣「 *BERITA MENU* 」⬣
+│❏ヅ ${prefix}covidglobal
+│❏ヅ ${prefix}infogempa
+│❏ヅ ${prefix}infogunungsumeru
+│❏ヅ ${prefix}infobanjir
+│❏ヅ ${prefix}infolongsor
+└⬣
+
+╭─⬣「 *WIBU MENU* 」⬣
+│❏ヅ ${prefix}loli
+│❏ヅ ${prefix}manga
+│❏ヅ ${prefix}anime 
+│❏ヅ ${prefix}lolivideo
+│❏ヅ ${prefix}husbu
+│❏ヅ ${prefix}waifu
+│❏ヅ ${prefix}milf
+│❏ヅ ${prefix}neko
+│❏ヅ ${prefix}kanna
+│❏ヅ ${prefix}sagiri
+│❏ヅ ${prefix}hentai
+│❏ヅ ${prefix}wallnime
+│❏ヅ ${prefix}kusonime
+│❏ヅ ${prefix}megumin
+│❏ヅ ${prefix}otakudesu
+│❏ヅ ${prefix}doujindesu
+│❏ヅ ${prefix}storyanime
+│❏ヅ ${prefix}otakuongoing
+│❏ヅ ${prefix}nhentai *code*
+│❏ヅ ${prefix}nekopoi *link*
+└⬣
+
+╭─⬣「 *RANDOM MENU* 」⬣
+│❏ヅ ${prefix}tourl
+│❏ヅ ${prefix}tinyurl
+│❏ヅ ${prefix}bilamgangka
+│❏ヅ ${prefix}artimimpi
+│❏ヅ ${prefix}resepmasakan
+└⬣
+
+╭─⬣「 *OWNER MENU* 」⬣
+│❏ヅ ${prefix}bc *teks*
+│❏ヅ ${prefix}term
+│❏ヅ ${prefix}eval
+│❏ヅ ${prefix}reset
+│❏ヅ ${prefix}clearall
+│❏ヅ ${prefix}leaveall
+│❏ヅ ${prefix}join *link gc*
+│❏ヅ ${prefix}shutdown
+│❏ヅ ${prefix}getquoted
+│❏ヅ ${prefix}addupdate *fiturnya*
+│❏ヅ ${prefix}exif *nama|author*
+│❏ヅ ${prefix}sewa add/del *waktunya*
+│❏ヅ ${prefix}premium add @tag|nomor
+│❏ヅ ${prefix}premium del @tag|nomor
+└⬣
+
+╭─⬣「 *SEND MENU* 」⬣
+│❏ヅ ${prefix}spam
+│❏ヅ ${prefix}tospam
+│❏ヅ ${prefix}spamsw
+└⬣
+
+╭─⬣「 *SEARCH MENU* 」⬣
+│❏ヅ ${prefix}nekopoisearch *query*
+│❏ヅ ${prefix}ytsearch *query*
+│❏ヅ ${prefix}shopee *product*
+│❏ヅ ${prefix}playstore *query*
+│❏ヅ ${prefix}ssweb *query*
+│❏ヅ ${prefix}google *query*
+│❏ヅ ${prefix}image *query*
+│❏ヅ ${prefix}pinterest *query*
+│❏ヅ ${prefix}apkpure *query*
+└⬣
+
+╭─⬣「 *FUN MENU* 」⬣
+│❏ヅ ${prefix}mining
+│❏ヅ ${prefix}cekwatak
+│❏ヅ ${prefix}cekmati [nama]
+│❏ヅ ${prefix}wangy [nama]
+│❏ヅ ${prefix}citacita
+│❏ヅ ${prefix}toxic
+│❏ヅ ${prefix}truth
+│❏ヅ ${prefix}dare
+│❏ヅ ${prefix}apakah
+│❏ヅ ${prefix}bisakah
+│❏ヅ ${prefix}kapankah
+│❏ヅ ${prefix}rate
+│❏ヅ ${prefix}jadian
+│❏ヅ ${prefix}cantik
+│❏ヅ ${prefix}ganteng
+│❏ヅ ${prefix}beban
+│❏ヅ ${prefix}babi
+│❏ヅ ${prefix}cekganteng
+│❏ヅ ${prefix}cekcantik
+└⬣
+
+╭─⬣「 *MEME MENU* 」⬣
+│❏ヅ ${prefix}meme
+│❏ヅ ${prefix}memeindo
+│❏ヅ ${prefix}darkjoke
+└⬣
+
+_Copyrigth © 2022 p3mu14-ui . all reverse_`}
---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'PROSES...',
+			success: 'DONE',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER NGAB',
+				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+			},
+			only: {
+				group: 'KHUSUS GRUP NGAB',
+				admin: 'KHUSUS ADMIN NGAB',
+				premium: 'LU BUKAN MEMBER PREMIUM NGAB',
+				owner: 'KHUSUS OWNER NGAB',
+				Badmin: 'BOT HARUS JADI ADMIN NGAB',
+			}
+		}
